fa05638399e3b80e54989300367c452ba4be286c
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-05-06 16:16:31 +0200

    Add MatButtonModule in MaterialComponents module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material';
+
+const MaterialComponents = [
+  MatButtonModule
+];
 
 @NgModule({
-  declarations: [],
-  imports: [],
-  exports: []
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
 })
 export class MaterialModule { }
