1c193df5d70ca6b839daf56c63bf528c23a9d655
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-05-09 13:07:56 +0200

    6 - Button Toggle: MatButtonToggleModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { TypographyComponent } from './tutorials/typography/typography.component';
 import { ButtonComponent } from './tutorials/button/button.component';
+import { ButtonToggleComponent } from './tutorials/button-toggle/button-toggle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TypographyComponent,
-    ButtonComponent
+    ButtonComponent,
+    ButtonToggleComponent
   ],
   imports: [
     BrowserModule,
