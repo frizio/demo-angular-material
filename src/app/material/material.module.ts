d759a4763f4ebade74289d9b3b1d2a7f4ad3774d
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-05-13 15:16:33 +0200

    Formatting material module imports

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,35 +1,38 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatButtonToggleModule,
-         MatIconModule, MatProgressSpinnerModule, MatProgressBarModule,
-         MatToolbarModule,
-         MatSidenavModule,
-         MatMenuModule,
-         MatListModule,
-         MatDividerModule,
-         MatGridListModule,
-         MatExpansionModule,
-         MatCardModule,
-         MatTabsModule,
-         MatStepperModule} from '@angular/material';
+import {
+  MatButtonModule, MatButtonToggleModule,
+
+  MatIconModule, MatProgressSpinnerModule, MatProgressBarModule,
+
+  MatToolbarModule, MatSidenavModule, MatMenuModule,
+
+  MatListModule, MatDividerModule, MatGridListModule, MatExpansionModule,
+  MatCardModule, MatTabsModule, MatStepperModule
+  } from '@angular/material';
 import { MatBadgeModule } from '@angular/material/badge';
 
 const MaterialComponents = [
   MatButtonModule,
   MatButtonToggleModule,
   MatIconModule,
+
   MatBadgeModule,
   MatProgressSpinnerModule,
   MatProgressBarModule,
+
   MatToolbarModule,
   MatSidenavModule,
   MatMenuModule,
+
   MatListModule,
   MatDividerModule,
   MatGridListModule,
   MatExpansionModule,
   MatCardModule,
   MatTabsModule,
-  MatStepperModule
+  MatStepperModule,
+
+
 ];
 
 @NgModule({
