98e46ef8843fc61347c4012c5c250eaaa4372c18
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-05-13 15:58:54 +0200

    21 - Autocomplete with array of options

--- src/app/forms/autocomplete/autocomplete.component.ts
+++ src/app/forms/autocomplete/autocomplete.component.ts
@@ -9,9 +9,20 @@ export class AutocompleteComponent implements OnInit {
 
   options = ['Angular', 'React', 'Vue'];
 
+  objectOptions = [
+    {name: 'Angular'},
+    {name: 'Angular Material'},
+    {name: 'React'},
+    {name: 'Vue'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  displayFunction(subject) {
+   return subject ? subject.name : undefined;
   }
 
 }
