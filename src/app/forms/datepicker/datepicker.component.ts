1278040bc2750239e9d14ed38bfea7f277952d70
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-05-14 11:20:55 +0200

    23 - Date Picker,  min, max,  matDatepickerFilter

--- src/app/forms/datepicker/datepicker.component.ts
+++ src/app/forms/datepicker/datepicker.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatepickerComponent implements OnInit {
 
+  minDate = new Date();
+  maxDate = new Date(2019, 8, 31);
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  dateFilter = (date: Date) => {
+    const day = date.getDay();
+    return day !== 0 && day !== 6;
   }
 
 }
